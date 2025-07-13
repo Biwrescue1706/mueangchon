5fa0d80397f0f66e1dd7bc74f219843ece0a91f6
Biwbong <bewrockgame1@gmail.com>
2025-07-13 11:46:16 +0700

    1

--- chon-backend/server.js
+++ chon-backend/server.js
@@ -44,4 +44,4 @@ app.post('/items', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:${PORT}`));
