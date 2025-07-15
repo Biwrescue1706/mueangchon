e275cfcc32d4f96ed0d628188f95964e89afee7a
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-07-15 15:26:20 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -30,7 +30,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     alert('เข้าสู่ระบบสำเร็จ!');
 
     // ทำอย่างอื่น เช่น redirect ไปหน้าอื่น
-    window.location.href = '/dashboard.html';
+    window.location.href = '/index.html';
 
   } catch (error) {
     console.error('Login failed:', error);
